adb07d8ef9b963eebe0c1c585444933e7cee26a4
Tobiloba <tobiemma200@gmail.com>
2022-12-27 11:25:25 +0100

    fix errors with the event handlers

--- src/helpers/eventHandlers.ts
+++ src/helpers/eventHandlers.ts
@@ -1,8 +1,8 @@
 // Handles messages events
-const handleMessage = (sender_psid, received_message) => {};
+const handleMessage = (sender_psid: string, received_message: string) => {};
 
 // Handles messaging_postbacks events
-const handlePostback = (sender_psid, received_postback) => {};
+const handlePostback = (sender_psid: string, received_postback: string) => {};
 
 // Sends response messages via the Send API
-const callSendAPI = (sender_psid, response) => {};
+const callSendAPI = (sender_psid: string, response: string) => {};
