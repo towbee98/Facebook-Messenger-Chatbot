e8ac790618145d0ef700a3818e76fd538725caa1
Tobiloba <tobiemma200@gmail.com>
2022-12-27 13:11:16 +0100

    updated the handlPostBack function

--- src/helpers/eventHandlers.ts
+++ src/helpers/eventHandlers.ts
@@ -11,6 +11,36 @@ const handleMessage = (sender_psid: string, received_message: any) => {
         response = {
             text: `You sent the message: "${received_message.text}". Now send me an image!`,
         };
+    } else if (received_message.attachments) {
+        // Gets the URL of the message attachment
+        let attachment_url = received_message.attachments[0].payload.url;
+        response = {
+            attachment: {
+                type: 'template',
+                payload: {
+                    template_type: 'generic',
+                    elements: [
+                        {
+                            title: 'Is this the right picture?',
+                            subtitle: 'Tap a button to answer.',
+                            image_url: attachment_url,
+                            buttons: [
+                                {
+                                    type: 'postback',
+                                    title: 'Yes!',
+                                    payload: 'yes',
+                                },
+                                {
+                                    type: 'postback',
+                                    title: 'No!',
+                                    payload: 'no',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+        };
     }
 
     // Sends the response message
@@ -18,7 +48,21 @@ const handleMessage = (sender_psid: string, received_message: any) => {
 };
 
 // Handles messaging_postbacks events
-const handlePostback = (sender_psid: string, received_postback: string) => {};
+const handlePostback = (sender_psid: string, received_postback: any) => {
+    let response;
+
+    // Get the payload for the postback
+    let payload = received_postback.payload;
+
+    // Set the response based on the postback payload
+    if (payload === 'yes') {
+        response = { text: 'Thanks!' };
+    } else if (payload === 'no') {
+        response = { text: 'Oops, try sending another image.' };
+    }
+    // Send the message to acknowledge the postback
+    callSendAPI(sender_psid, response);
+};
 
 // Sends response messages via the Send API
 const callSendAPI = (sender_psid: string, response: any) => {
